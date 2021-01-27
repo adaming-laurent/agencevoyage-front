99b95653822131206b3de66fee0d8424dae548c4
Guillaume VITTOZ <vittoz.guillaume@live.fr>
2021-01-27 17:10:35 +0100

    customer html + ts, save KO

---
+++ light-bootstrap-dashboard-angular2-master (1)/light-bootstrap-dashboard-angular2-master/src/app/service/customer.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+  private baseURL = "http://localhost:9090/customers";
+  constructor(private httpClient : HttpClient) { }
+  
+  public findAll(): Observable<any>{
+    return this.httpClient.get(this.baseURL);
+  }
+  public findOne(id:number): Observable<any>{
+    return this.httpClient.get(this.baseURL+"/"+id);
+  }
+  public save(customer:any): Observable<any>{
+    console.log(customer);
+    return this.httpClient.post(this.baseURL, customer);
+  }
+  public delete(id:number): Observable<any>{
+    return this.httpClient.delete(this.baseURL+"/"+id);
+  }
+}
--- light-bootstrap-dashboard-angular2-master (1)/light-bootstrap-dashboard-angular2-master/src/app/user/user.component.ts
+++ light-bootstrap-dashboard-angular2-master (1)/light-bootstrap-dashboard-angular2-master/src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from 'app/model/customer';
+import { CustomerService } from 'app/service/customer.service';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  customers: Customer[];
+  customer: Customer = new Customer();
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit() {
+    this.findAll();
+  }
+  findAll() {
+    this.customerService.findAll().subscribe(data => { this.customers = data });
+  }
+  // findOne(id){
+  //   this.userService.findOne(id).subscribe(data => )
+  //}
+  delete(customer) {
+    this.customerService.delete(customer.CustomerId).subscribe(
+      () => { this.findAll() }
+    );
+  }
+
+  save() {
+    this.customerService.save(this.customer).subscribe(
+      ()=>{this.findAll();
+      this.customer = new Customer();
+      }
+    )
   }
 
 }
